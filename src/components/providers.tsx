a5a08f8f4b2cb26b9f451b384dae7b567fb098f6
esau-morais <esaumorais7@gmail.com>
2023-10-13 10:15:17 -0300

    feat: init project

---
+++ src/components/providers.tsx
@@ -0,0 +1,9 @@
+// app/providers.tsx
+"use client";
+
+import { NextUIProvider } from "@nextui-org/react";
+import type { ReactNode } from "react";
+
+export function Providers({ children }: { children: ReactNode }) {
+  return <NextUIProvider>{children}</NextUIProvider>;
+}
